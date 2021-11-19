9ff53a3659344686b661bf9212cc90d90c6dd915
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-11-19 11:52:51 +0100

    fix test

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 // compare the two calls (handshake and api.audio) and return true if they are the same
-const { getScriptList, getMastering } = require("./lambda/util-api");
+const { getScriptJsonList, getMastering } = require("./lambda/util-api");
 const { getHandshakeResult, getScriptList } = require("./lambda/util");
 
 async function main(username, script) {
   let oldcall = await getHandshakeResult(username, script);
   let newcall = await getMastering(username, script);
-  let isEqual = JSON.stringify(Object.keys(oldcall)) === JSON.stringify(Object.keys(newcall));
+  console.log(await getScriptList())
+  console.log(await getScriptJsonList())
   console.log(oldcall);
   console.log(newcall);
   console.log(Object.keys(oldcall));
   console.log(Object.keys(newcall));
-  return 
+  return JSON.stringify(Object.keys(oldcall)) === JSON.stringify(Object.keys(newcall));
 }
 
 main(username="gggg", script="florence_final_alexa")
